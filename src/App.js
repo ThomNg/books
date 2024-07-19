43b2c04c2231dfd92fcb38f8b38e5d6fea33e20e
Thomas Ng <tng.ng.thomas@gmail.com>
2024-07-19 11:37:54 +0200

    App that list title of books

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
 
 function App() {
+  const[books,setBooks] = useState([]);
+
+  const deleteBookById = (id) => {
+    const updated = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updated);
+  };
+
+  const updateBookById = (id, title) => {
+    const updated = books.map ((book) =>{
+      if(book.id === id){
+        return {...book, title};
+      }
+      return book;
+    });
+    setBooks(updated);
+  }
+
+  const createBook = (title) => {
+    setBooks([...books, {id:Math.floor(Math.random()*9999), title}])
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="app">
+    <h1>Reading List</h1>
+    <BookList books={books} onDelete = {deleteBookById} onUpdate={updateBookById}/>
+    <BookCreate onCreate={createBook}/>
+  
+  </div>
+
   );
 }
 
