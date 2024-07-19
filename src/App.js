35f5493ba741b9c102d1c964148e0bf96c593087
Thomas Ng <tng.ng.thomas@gmail.com>
2024-07-19 23:23:04 +0200

    Adding persistent data with local JSON API server

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
+
 
 
 function App() {
   const[books,setBooks] = useState([]);
 
-  const deleteBookById = (id) => {
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  }
+
+  useEffect(()=>{
+    fetchBooks();
+  },[]);
+
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
+    
     const updated = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updated);
   };
 
-  const updateBookById = (id, title) => {
+  const updateBookById = async (id, title) => {
+
+    const response = await axios.put(`http://localhost:3001/books/${id}`,{
+      title
+    })
     const updated = books.map ((book) =>{
       if(book.id === id){
-        return {...book, title};
+        return {...book, ...response.data};
       }
       return book;
     });
     setBooks(updated);
   }
 
-  const createBook = (title) => {
-    setBooks([...books, {id:Math.floor(Math.random()*9999), title}])
+  const createBook = async (title) => { 
+    const response = await axios.post('http://localhost:3001/books',{
+      title,
+    });
+    setBooks([...books, response.data]);
   };
   
   return (
